13944fdda8cc0b67ca0f2d5beb2bdca57063f51c
Congyuwang <116020237@link.cuhk.edu.cn>
2020-05-17 13:47:16 +0800

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,7 @@
-import java.math.BigDecimal;
-import java.util.Scanner;
-import calculator.PreservedKeywordException;
 import calculator.Calculator;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("=================================================");
-        System.out.println("    This is a calculator program.\n" +
-                           "    Enter /help for help.\n" +
-                           "    Enter /exit to exit.");
-        System.out.println("=================================================");
-        Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
-        while (true) {
-            System.out.print(">> ");
-            String nextLine = scanner.nextLine();
-            if ("/exit".equals(nextLine)) {
-                System.out.println("Bye!");
-                scanner.close();
-                return;
-            }
-            if ("".equals(nextLine)) {
-                continue;
-            }
-            if ("/help".equals(nextLine)) {
-                System.out.println("================================================================================");
-                System.out.println("Help Menu\n" +
-                                   "\n" +
-                                   "This is a simple calculator program that allows variable definition, and\n" +
-                                   "supports basic functions like exponential, logarithm, and square root.\n" +
-                                   "\n" +
-                                   "Variable names must start with a latin letter, and follows latin letters or\n" +
-                                   "numbers and underscores. Examples: a1, number_of_apples, alpha__.\n" +
-                                   "\n" +
-                                   "Supported operators (in descending order of priority): () (parenthesis), !,\n" +
-                                   "+, - (logical_not, unary positive, unary negative), ^ (power) *, /, %\n" +
-                                   "(multiply, divide, remainder), +, - (plus, minus), > < >= <= (greater, less,\n" +
-                                   "greater equal, less equal), ==, != (equals, not equals), & (logical_and), |\n" +
-                                   "logical_or.\n" +
-                                   "\n" +
-                                   "Supported assignments: =, +=, -=, *=, /=, %= assignments\n" +
-                                   "\n" +
-                                   "Supported functions: sqrt() (square root), exp() (exponent), log() (natural\n" +
-                                   "logarithm), log10() (logarithm base 10),\n" +
-                                   "\n" +
-                                   "Predefined variables: e and pi, which can be reassigned if needed.\n");
-                System.out.println("================================================================================");
-                continue;
-            }
-            if (nextLine.charAt(0) == '/') {
-                System.out.println("Unknown command");
-                continue;
-            }
-            try {
-                BigDecimal result = calculator.expression(nextLine);
-                if (result != null) {
-                    System.out.println(result);
-                }
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            } catch (ArithmeticException e2) {
-                System.out.println(e2.getMessage());
-            } catch (PreservedKeywordException e3) {
-                System.out.println(e3.getMessage());
-            }
-        }
+        new Calculator();
     }
 }
